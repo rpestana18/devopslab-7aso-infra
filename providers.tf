e563558627f00805129f617f0762a9860b3631db
Rodrigo Pestana <58884259+rpestana18@users.noreply.github.com>
2022-06-14 22:03:37 -0300

    Create VM

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.24.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/home/vagrant/gcpkey.json")
+
+  project = "labdevopscloud7aso"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
