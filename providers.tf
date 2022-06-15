86b4452a89f6c9ef08e16011826a4435d6f30df1
Rodrigo Pestana <58884259+rpestana18@users.noreply.github.com>
2022-06-14 22:07:40 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/vagrant/gcpkey.json")
+  credentials = file("/workspace/vagrant/gcpkey.json")
 
   project = "labdevopscloud7aso"
   region  = "us-west1"
